8858c0168e4526c0db2f6c50fa8a3fc5c9560d01
lesandu1010 <nnimali968@gmail.com>
2025-01-03 19:11:46 +0530

    SADUWA-MD

--- settings.js
+++ settings.js
@@ -20,19 +20,19 @@
 
 //══════════════════════════════════════════════════════════════════════════════════════════════════════//
 //*
-//  * @project_name : MANU-MD
-//  * @author : Manul Official
-//  * @youtube : https://www.youtube.com/@ManulOfficialTech
-//  * @description : Manu-MD ,A Multi-functional whatsapp user bot.
+//  * @project_name : 𝚜𝚊𝚍𝚞𝚠𝚊 -𝙼𝙳
+//  * @author : 𝚂𝙰𝙳𝚄𝚆𝙰 Official
+//  * @youtube : 💗🫶
+//  * @description : 𝚂𝙰𝙳𝚄𝚆𝙰-𝙼𝙳 ,A Multi-functional whatsapp user bot.
 //*
 //*
-//base by Manul Official
-//GitHub: @manulofficial7
-//WhatsApp: +94742274855
+//base by 𝚂𝙰𝙳𝚄𝚆𝙰 Official
+//GitHub: 
+//WhatsApp: +947𝟽𝟺𝟽𝟼𝟾𝟸𝟽𝟿
 //want more free bot scripts? subscribe to my youtube channel: https://youtube.com/@ManulOfficialTech
-//   * Created By Github: manulofficial7.
+//   * Created By Github: 𝚂𝙰𝙳𝚄𝚆𝙰.
 //   * Credit To Manul Official
-//   * © 2024 MANU-MD-V7.
+//   * © 2024 𝚂𝙰𝙳𝚄𝚆𝙰-MD-V7.
 // ⛥┌┤
 // */
 
@@ -43,6 +43,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "put your session id", // ඔයාගෙ සෙසන් එක දාගන්න / Put Your Session_id Here !!! 💚
-MONGODB: process.env.MONGODB || "put your mongodb url", // ඔයාගෙ Mongodb Url එක දාගන්න / Put Your Mongodb Url Here !!! 💚
+SESSION_ID: process.env.SESSION_ID || "Manu-MD&PQUTxLAT#QaaTfD5gbeiRU7PCOr-07uAMXuuqWUMy5qxy_3y2LaQ", // ඔයාගෙ සෙසන් එක දාගන්න / Put Your Session_id Here !!! 💚
+MONGODB: process.env.MONGODB || "", // ඔයාගෙ Mongodb Url එක දාගන්න / Put Your Mongodb Url Here !!! 💚
 };
